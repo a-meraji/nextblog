ec8c7763e28c2e245dec098cfe76dfcd7679bd1b
amin <merajiamin1997@gmail.com>
2022-05-04 19:36:05 +0430

    sidebar only in home poge

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,14 +2,21 @@ import React from 'react'
 import FootNav from './FootNav'
 import Navbar from './Navbar'
 import SideSocials from './Sidebar'
+import { useRouter } from 'next/router'
 
 export default function Layout({ children }) {
+  const router = useRouter()
+
   return (
     <>
       {children}
       <Navbar />
-      <SideSocials/>
-      <FootNav/>
+      {router.pathname === '/' && (
+        <>
+          <SideSocials />
+          <FootNav />
+        </>
+      )}
     </>
   )
 }
