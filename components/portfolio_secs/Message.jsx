11aa50d728d9b8aeabee7d16a3fa8ff2e7d8424d
a-meraji <58762298+a-meraji@users.noreply.github.com>
2023-07-27 16:18:24 +0330

    Update Message.jsx

--- components/portfolio_secs/Message.jsx
+++ components/portfolio_secs/Message.jsx
@@ -3,6 +3,8 @@ import { IoSend } from 'react-icons/io5'
 import { BiLoader, BiCheck } from 'react-icons/bi'
 import { MdOutlineSmsFailed } from 'react-icons/md'
 import { useState } from 'react'
+import { socials } from '../../utils/assets'
+import Link from 'next/link'
 
 export default function Message() {
   const [isClicked, setIsClicked] = useState(false)
@@ -39,7 +41,7 @@ export default function Message() {
   }
 
   return (
-    <section className="mt-8 flex h-screen flex-col items-center justify-center">
+    <section className="mt-36 flex h-screen flex-col items-center justify-center">
       <h1 className="mb-8  text-center text-4xl">Let's talk</h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -50,7 +52,7 @@ export default function Message() {
           className="mb-4 w-full rounded-full bg-transAccent py-2 px-4 focus:outline-none"
           {...register('userName', { required: true })}
         />
-        {errors.userName && <p>Name is required.</p>}
+        {errors.userName && <p className='-mt-3 mb-2'>Name is required.</p>}
         <input
           placeholder="Your E-mail"
           className="my-4 w-full rounded-full bg-transAccent py-2 px-4 focus:outline-none"
@@ -61,7 +63,7 @@ export default function Message() {
           })}
         />
         {errors.userEmail && (
-          <p>
+          <p className='-mt-3 mb-2'>
             {errors.userEmail.type == 'required'
               ? 'Please enter your e-mail.'
               : 'Invalid e-mail'}
@@ -72,7 +74,7 @@ export default function Message() {
           className="my-4 h-[40vh] w-full rounded-2xl bg-transAccent py-2 px-4 focus:outline-none"
           {...register('userMessage', { required: true })}
         />
-        {errors.userMessage && <p>Don't be shy! Say something.</p>}
+        {errors.userMessage && <p className='-mt-3 mb-4'>Don't be shy! Say something.</p>}
         <button
           type="submit"
           className={`group relative flex items-center justify-between gap-x-1 rounded-full bg-accent py-2 text-onAccent transition-all ${
@@ -128,6 +130,20 @@ export default function Message() {
           </span>
         </button>
       </form>
+      <div>
+        <h6 className='text-2xl mt-20'>Or you can contact me with these options</h6>
+        <div className="flex w-full justify-center gap-x-8 items-center mt-10 text-accent">
+        {socials.map((social, index) => index>1? (
+          <div key={`${social.name}${index}`}>
+            <Link href={social.url}>
+              <a target="_blank">
+                <social.icon className="h-6 w-6" />
+              </a>
+            </Link>
+          </div>
+        ):<></>)}
+      </div>
+      </div>
       <style jsx>{`
         p {
           color: red;
